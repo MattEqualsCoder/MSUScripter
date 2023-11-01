a8ea3590d23f02220e45ae0b52f417317dc891f0
MattEqualsCoder <mattequalscoder@gmail.com>
2023-11-01 12:44:39 -0400

    Update to create output folder

--- Setup/LinuxBuildZipper.ps1
+++ Setup/LinuxBuildZipper.ps1
@@ -1,27 +1,32 @@
 $parentFolder = Split-Path -parent $PSScriptRoot
 
 # Get publish folder
-$folder = "$parentFolder\MSUScripter\bin\Release\net7.0\linux-x64\publish"
-$winFolder = "$parentFolder\MSUScripter\bin\Release\net7.0-windows\win-x86\publish"
+$folder = "$parentFolder/MSUScripter/bin/Release/net7.0/linux-x64/publish"
+$winFolder = "$parentFolder/MSUScripter/bin/Release/net7.0-windows/win-x86/publish"
 if (-not (Test-Path $folder))
 {
-    $folder = "$parentFolder\MSUScripter\bin\Release\net7.0\publish\linux-x64"
-    $winFolder = "$parentFolder\MSUScripter\bin\Release\net7.0-windows\publish\win-x86"
+    $folder = "$parentFolder/MSUScripter/bin/Release/net7.0/publish/linux-x64"
+    $winFolder = "$parentFolder/MSUScripter/bin/Release/net7.0-windows/publish/win-x86"
 }
 
 # Get version number from MSUScripter
 $version = "0.0.0"
-if (Test-Path "$winFolder\MSUScripter.exe") {
-    $version = (Get-Item "$winFolder\MSUScripter.exe").VersionInfo.ProductVersion
+if (Test-Path "$winFolder/MSUScripter.exe") {
+    $version = (Get-Item "$winFolder/MSUScripter.exe").VersionInfo.ProductVersion
 }
 else {
-    $version = (Get-Item "$folder\MSUScripter.dll").VersionInfo.ProductVersion
+    $version = (Get-Item "$folder/MSUScripter.dll").VersionInfo.ProductVersion
 }
 Write-Host "Version: $version"
 
 # Create package
 $fullVersion = "MSUScripterLinux_$version"
-$outputFile = "$PSScriptRoot/Output/$fullVersion.tar.gz"
+$outputFolder = "$PSScriptRoot/Output"
+if (-not Test-Path $outputFolder)
+{
+    New-Item -Path "$PSScriptRoot" -Name "Output" -ItemType Directory
+}
+$outputFile = "$outputFolder/$fullVersion.tar.gz"
 Write-Host "Creating $outputFile"
 if (Test-Path $outputFile) {
     Remove-Item $outputFile -Force
