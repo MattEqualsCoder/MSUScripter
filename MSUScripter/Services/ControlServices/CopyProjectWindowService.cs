68753e9983853369707c3e18f385bc68f8b5a04b
MattEqualsCoder <mattequalscoder@gmail.com>
2024-07-09 07:55:01 -0400

    Updated CopyProjectWindow

---
+++ MSUScripter/Services/ControlServices/CopyProjectWindowService.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia.Platform.Storage;
+using AvaloniaControls;
+using AvaloniaControls.ControlServices;
+using MSUScripter.Configs;
+using MSUScripter.ViewModels;
+
+namespace MSUScripter.Services.ControlServices;
+
+public class CopyProjectWindowService(ConverterService converterService) : ControlService
+{
+    private CopyProjectWindowViewModel _model = new();
+
+    public CopyProjectWindowViewModel InitializeModel()
+    {
+        return _model;
+    }
+
+    public void SetProject(MsuProject project)
+    {
+        _model.OriginalProject = project;
+        _model.ProjectViewModel = converterService.ConvertProject(project);
+        
+        var paths = new List<CopyProjectViewModel>
+        {
+            new(project.ProjectFilePath),
+            new(project.MsuPath)
+        };
+
+        if (!string.IsNullOrEmpty(project.BasicInfo.ZeldaMsuPath))
+        {
+            paths.Add(new CopyProjectViewModel(project.BasicInfo.ZeldaMsuPath));
+        }
+        
+        if (!string.IsNullOrEmpty(project.BasicInfo.MetroidMsuPath))
+        {
+            paths.Add(new CopyProjectViewModel(project.BasicInfo.MetroidMsuPath));
+        }
+        
+        foreach (var song in project.Tracks.SelectMany(x => x.Songs))
+        {
+            foreach (var file in song.MsuPcmInfo.GetFiles())
+            {
+                if (paths.Any(x => x.NewPath == file))
+                {
+                    continue;
+                }
+                paths.Add(new CopyProjectViewModel(file));
+            }
+            
+        }
+
+        _model.Paths = paths;
+        CheckFiles();
+    }
+    
+    public async Task UpdatePath(CopyProjectViewModel viewModel, IStorageItem file)
+    {
+        viewModel.NewPath = file.Path.LocalPath;
+
+        var folderPath = (await file.GetParentAsync())!.Path.LocalPath;
+        foreach (var folderFile in Directory.GetFiles(folderPath))
+        {
+            var folderFileInfo = new FileInfo(folderFile);
+            
+            var otherViewModel = _model.Paths.FirstOrDefault(x => x != viewModel && x.BaseFileName == folderFileInfo.Name && x.PreviousPath == x.NewPath);
+            if (otherViewModel == null)
+            {
+                continue;
+            }
+
+            otherViewModel.NewPath = folderFile;
+        }
+
+        CheckFiles();
+    }
+
+    public void ImportProject()
+    {
+        if (_model.ProjectViewModel == null)
+        {
+            return;
+        }
+
+        _model.ProjectViewModel.ProjectFilePath = _model.Paths
+            .First(x => x.Extension.Equals(".msup", StringComparison.OrdinalIgnoreCase)).NewPath;
+
+        foreach (var path in _model.Paths.Where(x => x.Extension.Equals(".msu", StringComparison.OrdinalIgnoreCase)))
+        {
+            if (_model.ProjectViewModel.MsuPath == path.PreviousPath)
+            {
+                _model.ProjectViewModel.MsuPath = path.NewPath;
+            }
+            else if (_model.ProjectViewModel.BasicInfo.MetroidMsuPath == path.PreviousPath)
+            {
+                _model.ProjectViewModel.BasicInfo.MetroidMsuPath = path.NewPath;
+            }
+            else if (_model.ProjectViewModel.BasicInfo.ZeldaMsuPath == path.PreviousPath)
+            {
+                _model.ProjectViewModel.BasicInfo.ZeldaMsuPath = path.NewPath;
+            }
+        }
+        
+        var oldMsuPath = _model.OriginalProject?.MsuPath.Replace(".msu", "", StringComparison.OrdinalIgnoreCase) ?? "";
+        var newMsuPath = _model.ProjectViewModel?.MsuPath.Replace(".msu", "", StringComparison.OrdinalIgnoreCase) ?? "";
+        
+        foreach (var path in _model.Paths.Where(x => !x.Extension.Equals(".msu", StringComparison.OrdinalIgnoreCase) && !x.Extension.Equals(".msup", StringComparison.OrdinalIgnoreCase)))
+        {
+            foreach (var song in _model.ProjectViewModel!.Tracks.SelectMany(x => x.Songs))
+            {
+                UpdateSongPaths(song, path, oldMsuPath, newMsuPath);
+            }
+        }
+
+        _model.NewProject = converterService.ConvertProject(_model.ProjectViewModel!);
+        return;
+    }
+    
+    private void UpdateSongPaths(MsuSongInfoViewModel song, CopyProjectViewModel update, string oldMsuPath, string newMsuPath)
+    {
+        song.OutputPath = song.OutputPath?.Replace(oldMsuPath, newMsuPath);
+        if (song.MsuPcmInfo.HasFiles())
+        {
+            UpdateMsuPcmInfo(song.MsuPcmInfo, update);    
+        }
+    }
+
+    private void UpdateMsuPcmInfo(MsuSongMsuPcmInfoViewModel pcmInfo, CopyProjectViewModel update)
+    {
+        pcmInfo.File = pcmInfo.File?.Replace(update.PreviousPath, update.NewPath);
+        foreach (var subchannel in pcmInfo.SubChannels)
+        {
+            UpdateMsuPcmInfo(subchannel, update);
+        }
+        foreach (var subtrack in pcmInfo.SubTracks)
+        {
+            UpdateMsuPcmInfo(subtrack, update);
+        }
+    }
+    
+    private void CheckFiles()
+    {
+        foreach (var path in _model.Paths)
+        {
+            if (path.Extension.Equals(".msup", StringComparison.OrdinalIgnoreCase) ||
+                path.Extension.Equals(".msu", StringComparison.OrdinalIgnoreCase))
+            {
+                path.IsValid = !File.Exists(path.NewPath);
+                path.Message = path.IsValid ? "" : "File already exists";
+            }
+            else
+            {
+                path.IsValid = File.Exists(path.NewPath);
+                path.Message = path.IsValid ? "" : "File does not exist";
+            }
+        }
+
+        _model.IsValid = _model.Paths.All(x => x.IsValid);
+    }
+    
+}
--- MSUScripter/Views/CopyProjectWindow.axaml.cs
+++ MSUScripter/Views/CopyProjectWindow.axaml.cs
@@ -1,67 +1,40 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-using Avalonia.Platform.Storage;
+using AvaloniaControls;
 using AvaloniaControls.Controls;
+using AvaloniaControls.Extensions;
 using MSUScripter.Configs;
-using MSUScripter.Services;
+using MSUScripter.Services.ControlServices;
 using MSUScripter.ViewModels;
 
 namespace MSUScripter.Views;
 
 public partial class CopyProjectWindow : ScalableWindow
 {
-    private CopyProjectWindowViewModel Model = new();
+    private readonly CopyProjectWindowViewModel _model = new();
+    private readonly CopyProjectWindowService? _service;
     
     public CopyProjectWindow()
     {
         InitializeComponent();
-        DataContext = Model;
+
+        if (Design.IsDesignMode)
+        {
+            DataContext = new CopyProjectWindowViewModel().DesignerExample();
+        }
+        else
+        {
+            _service = this.GetControlService<CopyProjectWindowService>();
+            DataContext = _model = _service?.InitializeModel() ?? _model;
+        }
     }
 
     public async Task<MsuProject?> ShowDialog(Window parentWindow, MsuProject project)
     {
-        Model.OriginalProject = project;
-        Model.ProjectViewModel = ConverterService.Instance.ConvertProject(project);
-        
-        var paths = new List<CopyProjectViewModel>();
-
-        paths.Add(new CopyProjectViewModel(project.ProjectFilePath));
-
-        paths.Add(new CopyProjectViewModel(project.MsuPath));
-
-        if (!string.IsNullOrEmpty(project.BasicInfo.ZeldaMsuPath))
-        {
-            paths.Add(new CopyProjectViewModel(project.BasicInfo.ZeldaMsuPath));
-        }
-        
-        if (!string.IsNullOrEmpty(project.BasicInfo.MetroidMsuPath))
-        {
-            paths.Add(new CopyProjectViewModel(project.BasicInfo.MetroidMsuPath));
-        }
-        
-        foreach (var song in project.Tracks.SelectMany(x => x.Songs))
-        {
-            foreach (var file in song.MsuPcmInfo.GetFiles())
-            {
-                if (paths.Any(x => x.NewPath == file))
-                {
-                    continue;
-                }
-                paths.Add(new CopyProjectViewModel(file));
-            }
-            
-        }
-
-        Model.Paths = paths;
-        CheckFiles();
-        
+        _service?.SetProject(project);
         await ShowDialog(parentWindow);
-        return Model.NewProject;
+        return _model.NewProject;
     }
 
     private void UpdatePathButton_OnClick(object? sender, RoutedEventArgs e)
@@ -76,83 +49,19 @@ public partial class CopyProjectWindow : ScalableWindow
 
     private async Task UpdatePath(CopyProjectViewModel viewModel)
     {
-        var pattern = string.IsNullOrEmpty(viewModel.Extension)
-            ? null
-            : new List<FilePickerFileType>()
-            {
-                new($"{viewModel.Extension} File") { Patterns = new List<string>() { $"*{viewModel.Extension}" } }
-            };
-
-
-        IStorageFile? file;
+        var file = await CrossPlatformTools.OpenFileDialogAsync(
+            parentWindow: this,
+            type: viewModel.IsSongFile ? FileInputControlType.OpenFile : FileInputControlType.SaveFile,
+            filter: $"{viewModel.Extension} File:*{viewModel.Extension}",
+            path: viewModel.PreviousPath,
+            title: $"Select Replacement File for {viewModel.BaseFileName}");
         
-        if (viewModel.Extension.Equals(".msup", StringComparison.OrdinalIgnoreCase) ||
-            viewModel.Extension.Equals(".msu", StringComparison.OrdinalIgnoreCase))
+        if (string.IsNullOrEmpty(file?.Path.LocalPath))
         {
-            file = await StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions()
-            {
-                Title = $"Select Replacement File for {viewModel.BaseFileName}",
-                FileTypeChoices = pattern,
-            });
-
-            if (string.IsNullOrEmpty(file?.Path.LocalPath))
-            {
-                return;
-            }
-        }
-        else
-        {
-            var files = await StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions()
-            {
-                Title = $"Select Replacement File for {viewModel.BaseFileName}",
-                FileTypeFilter = pattern,
-            });
-
-            if (string.IsNullOrEmpty(files.FirstOrDefault()?.Path.LocalPath))
-            {
-                return;
-            }
-
-            file = files.First();
-        }
-        
-        viewModel.NewPath = file.Path.LocalPath;
-
-        var folderPath = (await file.GetParentAsync())!.Path.LocalPath;
-        foreach (var folderFile in Directory.GetFiles(folderPath))
-        {
-            var folderFileInfo = new FileInfo(folderFile);
-            
-            var otherViewModel = Model.Paths.FirstOrDefault(x => x != viewModel && x.BaseFileName == folderFileInfo.Name && x.PreviousPath == x.NewPath);
-            if (otherViewModel == null)
-            {
-                continue;
-            }
-
-            otherViewModel.NewPath = folderFile;
+            return;
         }
 
-        CheckFiles();
-    }
-
-    private void CheckFiles()
-    {
-        foreach (var path in Model.Paths)
-        {
-            if (path.Extension.Equals(".msup", StringComparison.OrdinalIgnoreCase) ||
-                path.Extension.Equals(".msu", StringComparison.OrdinalIgnoreCase))
-            {
-                path.IsValid = !File.Exists(path.NewPath);
-                path.Message = path.IsValid ? "" : "File already exists";
-            }
-            else
-            {
-                path.IsValid = File.Exists(path.NewPath);
-                path.Message = path.IsValid ? "" : "File does not exist";
-            }
-        }
-
-        Model.IsValid = Model.Paths.All(x => x.IsValid);
+        _service?.UpdatePath(viewModel, file);
     }
 
     private void CloseButton_OnClick(object? sender, RoutedEventArgs e)
@@ -162,66 +71,8 @@ public partial class CopyProjectWindow : ScalableWindow
 
     private void ImportProjectButton_OnClick(object? sender, RoutedEventArgs e)
     {
-        if (Model.ProjectViewModel == null)
-        {
-            return;
-        }
-
-        Model.ProjectViewModel.ProjectFilePath = Model.Paths
-            .First(x => x.Extension.Equals(".msup", StringComparison.OrdinalIgnoreCase)).NewPath;
-
-        foreach (var path in Model.Paths.Where(x => x.Extension.Equals(".msu", StringComparison.OrdinalIgnoreCase)))
-        {
-            if (Model.ProjectViewModel.MsuPath == path.PreviousPath)
-            {
-                Model.ProjectViewModel.MsuPath = path.NewPath;
-            }
-            else if (Model.ProjectViewModel.BasicInfo.MetroidMsuPath == path.PreviousPath)
-            {
-                Model.ProjectViewModel.BasicInfo.MetroidMsuPath = path.NewPath;
-            }
-            else if (Model.ProjectViewModel.BasicInfo.ZeldaMsuPath == path.PreviousPath)
-            {
-                Model.ProjectViewModel.BasicInfo.ZeldaMsuPath = path.NewPath;
-            }
-        }
-        
-        var oldMsuPath = Model.OriginalProject?.MsuPath.Replace(".msu", "", StringComparison.OrdinalIgnoreCase) ?? "";
-        var newMsuPath = Model.ProjectViewModel?.MsuPath.Replace(".msu", "", StringComparison.OrdinalIgnoreCase) ?? "";
-        
-        foreach (var path in Model.Paths.Where(x => !x.Extension.Equals(".msu", StringComparison.OrdinalIgnoreCase) && !x.Extension.Equals(".msup", StringComparison.OrdinalIgnoreCase)))
-        {
-            foreach (var song in Model.ProjectViewModel!.Tracks.SelectMany(x => x.Songs))
-            {
-                UpdateSongPaths(song, path, oldMsuPath, newMsuPath);
-            }
-        }
-
-        Model.NewProject = ConverterService.Instance.ConvertProject(Model.ProjectViewModel!);
-
+        _service?.ImportProject();
         Close();
     }
 
-    private void UpdateSongPaths(MsuSongInfoViewModel song, CopyProjectViewModel update, string oldMsuPath, string newMsuPath)
-    {
-        song.OutputPath = song.OutputPath?.Replace(oldMsuPath, newMsuPath);
-        if (song.MsuPcmInfo.HasFiles())
-        {
-            UpdateMsuPcmInfo(song.MsuPcmInfo, update);    
-        }
-    }
-
-    private void UpdateMsuPcmInfo(MsuSongMsuPcmInfoViewModel pcmInfo, CopyProjectViewModel update)
-    {
-        pcmInfo.File = pcmInfo.File?.Replace(update.PreviousPath, update.NewPath);
-        foreach (var subchannel in pcmInfo.SubChannels)
-        {
-            UpdateMsuPcmInfo(subchannel, update);
-        }
-        foreach (var subtrack in pcmInfo.SubTracks)
-        {
-            UpdateMsuPcmInfo(subtrack, update);
-        }
-    }
-    
 }
